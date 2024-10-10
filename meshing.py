aced888a408b3768b65b42457319590bd74074de
Federico Stella <federico.stella95@gmail.com>
2024-10-10 16:12:53 +0200

    Added batching

--- meshing.py
+++ meshing.py
@@ -1,5 +1,5 @@
 
-from utils import get_query_points
+from utils import get_query_points, df_and_grad_to_input_cells
 import utils
 import sys
 sys.path.append("custom_mc")
@@ -13,60 +13,17 @@ import trimesh
 POWERS_OF_2 = utils.POWERS_OF_2
 
 
-def df_and_grad_to_input_cells(udf, grads, max_avg_distance=None, max_max_distance=None):
-    """ Builds inputs cells for the network from the UDF and gradients. Each cell has 8 corners."""
-
-    n_grid_samples = udf.shape[0]
-
-    length = (n_grid_samples - 1) * (n_grid_samples - 1) * (n_grid_samples - 1)
-    shape = (n_grid_samples - 1, n_grid_samples - 1, n_grid_samples - 1)
-
-    udf_cells = torch.zeros((8, length))
-    grad_cells = torch.zeros((8, length, 3))
-    if (max_avg_distance is not None and max_max_distance is not None):
-        z_indices, y_indices, x_indices = np.unravel_index(np.arange(length), shape)
-        
-        udf_cells[0] = udf[z_indices, y_indices, x_indices]
-        udf_cells[1] = udf[z_indices, y_indices, x_indices + 1]
-        udf_cells[2] = udf[z_indices, y_indices + 1, x_indices + 1]
-        udf_cells[3] = udf[z_indices, y_indices + 1, x_indices]
-        udf_cells[4] = udf[z_indices + 1, y_indices, x_indices]
-        udf_cells[5] = udf[z_indices + 1, y_indices, x_indices + 1]
-        udf_cells[6] = udf[z_indices + 1, y_indices + 1, x_indices + 1]
-        udf_cells[7] = udf[z_indices + 1, y_indices + 1, x_indices]
-
-        # Do the same for gradients
-        grad_cells[0] = grads[z_indices, y_indices, x_indices]
-        grad_cells[1] = grads[z_indices, y_indices, x_indices + 1]
-        grad_cells[2] = grads[z_indices, y_indices + 1, x_indices + 1]
-        grad_cells[3] = grads[z_indices, y_indices + 1, x_indices]
-        grad_cells[4] = grads[z_indices + 1, y_indices, x_indices]
-        grad_cells[5] = grads[z_indices + 1, y_indices, x_indices + 1]
-        grad_cells[6] = grads[z_indices + 1, y_indices + 1, x_indices + 1]
-        grad_cells[7] = grads[z_indices + 1, y_indices + 1, x_indices]
-
-        within_thresholds = ((max_avg_distance is not None and max_max_distance is not None) and
-                            (udf_cells.mean(dim=0) <= max_avg_distance) &
-                            (udf_cells.max(dim=0).values <= max_max_distance))
-        
-        # Extract indices at which within_thresholds is True
-        indices = torch.nonzero(within_thresholds).squeeze(1).int()
-
-    else:
-        indices = torch.arange(0, length, 1)
-
-    return udf_cells, grad_cells, indices
-        
-
-
-
-def compute_pseudo_sdf(model, udf_and_grad_f, n_grid_samples=128):
+def compute_pseudo_sdf(model, udf_and_grad_f, n_grid_samples=128, batch_size=10000, normalize_udf=True, use_grads=True, out7=False):
     """Computes the pseudo-sdf of a mesh using a neural network model and a UDF and gradient function.
     
     Args:
         model: The neural network model.
         udf_and_grad_f: A function that takes query points and returns the UDF and gradients.
         n_grid_samples: The number of grid samples in each dimension.
+        batch_size: The batch size to use when computing the pseudo-sdf. You can play with this if you run of out memory.
+        normalize_udf: Whether to normalize the UDF values with the voxel size. This depends on how the model was trained. Change this only if you train the model with a different setting for this parameter.
+        use_grads: Whether to use gradients in the pseudo-sdf computation. This depends on how the model was trained. Change this only if you train the model with a different setting for this parameter.
+        out7: Whether the model has 7 outputs (compared to the default 128). This depends on how the model was trained. Change this only if you train the model with a different setting for this parameter.
     """
     device = next(model.parameters()).device
 
@@ -98,25 +55,36 @@ def compute_pseudo_sdf(model, udf_and_grad_f, n_grid_samples=128):
 
         pseudo_sdf = torch.zeros(shape + (8,))
 
+        cell_size = 32 if use_grads else 8
+
         # Build the input by putting the cells together
-        input = torch.zeros((len(indices), 32))
+        input = torch.zeros((len(indices), cell_size))
         input[:,:8] = udf_cells[:,indices].T
-        input[:,8:] = grad_cells[:,indices,:].permute(1,0,2).reshape(-1,24)
+        if use_grads:
+            input[:,8:] = grad_cells[:,indices,:].permute(1,0,2).reshape(-1,24)
 
 
         udf2 = input[:,:8].clone()
         udf2 = udf2.to(device)
 
         # Normalize the UDF values with the voxel size. This is key for the network to work at multiple resolutions.
-        input[:,:8] = input[:,:8] / voxel_size
+        if normalize_udf:
+            input[:,:8] = input[:,:8] / voxel_size
 
         input = input.to(device)
 
-
-        output = model(input)
+        output = torch.zeros((input.shape[0],7 if out7 else 128)).to(device)
+        batch_head = 0
+        while batch_head < input.shape[0]:
+            batch_tail = min(batch_head + batch_size, input.shape[0])
+            output[batch_head:batch_tail] = model(input[batch_head:batch_tail])
+            batch_head += batch_size
 
         # Compute the pseudo-signs based on the network's output configuration
-        pseudo_signs = output.argmax(axis=1).unsqueeze(-1).bitwise_and(POWERS_OF_2.to(device)).ne(0).int()	
+        if out7:
+            pseudo_signs = (torch.sigmoid(output) > 0.5).int()
+        else:
+            pseudo_signs = output.argmax(axis=1).unsqueeze(-1).bitwise_and(POWERS_OF_2.to(device)).ne(0).int()	
         pseudo_signs[pseudo_signs == 0] = -1
 
         # Multiply the pseudo-signs with the udf values to get a pseudo-sdf
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import torch
 import models
+import numpy as np
 
 POWERS_OF_2 = 2**torch.arange(7)
 
@@ -20,3 +21,49 @@ def load_model(model_path, device):
     model.to(device)
     model.eval()
     return model
+
+def df_and_grad_to_input_cells(udf, grads, max_avg_distance=None, max_max_distance=None):
+    """ Builds inputs cells for the network from the UDF and gradients, and filters out cells that are far from the surface. Each cell has 8 corners.
+        Return a list of udf cell, a list of gradient cells, and a list of indices of the cells that are within the thresholds."""
+
+    n_grid_samples = udf.shape[0]
+
+    length = (n_grid_samples - 1) * (n_grid_samples - 1) * (n_grid_samples - 1)
+    shape = (n_grid_samples - 1, n_grid_samples - 1, n_grid_samples - 1)
+
+    udf_cells = torch.zeros((8, length))
+    grad_cells = torch.zeros((8, length, 3))
+    if (max_avg_distance is not None and max_max_distance is not None):
+        z_indices, y_indices, x_indices = np.unravel_index(np.arange(length), shape)
+        
+        udf_cells[0] = udf[z_indices, y_indices, x_indices]
+        udf_cells[1] = udf[z_indices, y_indices, x_indices + 1]
+        udf_cells[2] = udf[z_indices, y_indices + 1, x_indices + 1]
+        udf_cells[3] = udf[z_indices, y_indices + 1, x_indices]
+        udf_cells[4] = udf[z_indices + 1, y_indices, x_indices]
+        udf_cells[5] = udf[z_indices + 1, y_indices, x_indices + 1]
+        udf_cells[6] = udf[z_indices + 1, y_indices + 1, x_indices + 1]
+        udf_cells[7] = udf[z_indices + 1, y_indices + 1, x_indices]
+
+        # Do the same for gradients
+        grad_cells[0] = grads[z_indices, y_indices, x_indices]
+        grad_cells[1] = grads[z_indices, y_indices, x_indices + 1]
+        grad_cells[2] = grads[z_indices, y_indices + 1, x_indices + 1]
+        grad_cells[3] = grads[z_indices, y_indices + 1, x_indices]
+        grad_cells[4] = grads[z_indices + 1, y_indices, x_indices]
+        grad_cells[5] = grads[z_indices + 1, y_indices, x_indices + 1]
+        grad_cells[6] = grads[z_indices + 1, y_indices + 1, x_indices + 1]
+        grad_cells[7] = grads[z_indices + 1, y_indices + 1, x_indices]
+
+        within_thresholds = ((max_avg_distance is not None and max_max_distance is not None) and
+                            (udf_cells.mean(dim=0) <= max_avg_distance) &
+                            (udf_cells.max(dim=0).values <= max_max_distance))
+        
+        # Extract indices at which within_thresholds is True
+        indices = torch.nonzero(within_thresholds).squeeze(1).int()
+
+    else:
+        indices = torch.arange(0, length, 1)
+
+    return udf_cells, grad_cells, indices
+        
